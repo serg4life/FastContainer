c583021ad650e6e4275822416f76faa14666531f
Sergio Fernandez <serg4life@duck.com>
2026-05-01 14:44:03 +0200

    OPTIMIZATION: doesnt rebuild always the image

--- run_docker.sh
+++ run_docker.sh
@@ -7,9 +7,26 @@ print_help() {
     echo "Example:  $0 my_container"
 }
 
+build_image() {
+    local container_name=$1
+    echo "Building Docker image for container '${container_name}'"
+    docker build -t ${container_name} ./${container_name}
+}
+
+run_image() {
+    local container_name=$1
+    echo "Running Docker container '${container_name}'"
+    docker run -v ./${container_name}/workspace:/workspace -it --rm ${container_name}
+}
+
 run_docker() {
     local container_name=$1
-    docker build -t ${container_name} ./${container_name} && docker run -v ./${container_name}/workspace:/workspace -it --rm ${container_name}
+
+     if docker image ls | grep -q $container_name; then
+        run_image $container_name
+    else
+        build_image $container_name && run_image $container_name
+    fi
 }
 
 if [ $# -ne 1 ]; then
