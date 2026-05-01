71a03fa4904f38ee952db791bb1e5670f88b6775
Sergio Fernandez <serg4life@duck.com>
2026-05-01 20:19:58 +0200

    user and permissions improvements

--- generic-dev/container-init/entrypoint.sh
+++ generic-dev/container-init/entrypoint.sh
@@ -16,6 +16,25 @@ on_error() {
     debug_prompt || exit 1
 }
 
+USER_NAME=developer
+USER_ID=${LOCAL_UID:-1000}
+GROUP_ID=${LOCAL_GID:-1000}
+
+# comprobar si ya existe un usuario con ese UID
+EXISTING_USER=$(getent passwd "$USER_ID" | cut -d: -f1)
+
+if [ -n "$EXISTING_USER" ]; then
+    # Si existe, cambiar su nombre y grupo al nuevo usuario
+    usermod -l "$USER_NAME" "$EXISTING_USER" 2>/dev/null || true
+    groupmod -n "$USER_NAME" "$EXISTING_USER" 2>/dev/null || true
+else
+    # Si no existe un usuario con ese UID, crear uno nuevo
+    groupadd -g "$GROUP_ID" "$USER_NAME" 2>/dev/null || true
+    useradd -m -u "$USER_ID" -g "$GROUP_ID" -s /bin/bash "$USER_NAME"
+fi
+
+echo "$USER_NAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/developer
+
 echo "Instalando..."
 
 if /container-init/install.sh; then
@@ -24,4 +43,4 @@ else
     debug_prompt
 fi
 
-exec "$@"
+exec gosu "$USER_NAME" "$@"
